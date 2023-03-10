60bb1e36bcb79bc8a6f5e2975061cbb1303fe23e
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-03-10 19:19:33 +1000

    added seminar03

---
+++ src/main/java/seminar03/Main.java
@@ -0,0 +1,155 @@
+package seminar03;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Object[] i;
+//
+//        ArrayList list = new ArrayList();
+//        list.add(4);
+//        list.add(8);
+//        list.add(12);
+//        list.add("Привет");
+//        list.add(false);
+//        list.add(.5f);
+//        list.add('d');
+//        list.remove(1);
+//        System.out.println(list);
+//
+//        if (list.get(1) instanceof String) ((String) list.get(1)).toUpperCase();
+
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("Привет");
+//        list.add("Синий");
+//        list.add("Берег");
+//        list.add("Рот");
+//        list.add("Ствол");
+//
+//        list.remove(1);
+////        System.out.println(list);
+//        list.add(list.size() / 2, "Стол");
+////        System.out.println(list);
+//        list.set(list.size() / 2, "Стол2");
+////        System.out.println(list);
+//
+//        ArrayList<String> strings = new ArrayList<>(list);
+//        strings.addAll(list);
+////        System.out.println(strings);
+//        strings.clear();
+////        System.out.println(strings);
+//
+////        list.forEach(n -> System.out.print(n + ", "));
+//        System.out.println();
+//        System.out.println(list.toString()
+//                .replace("[", "")
+//                .replace("]", ""));
+//
+//        Collections.sort(list);
+//        System.out.println(list.toString()
+//                .replace("[", "")
+//                .replace("]", ""));
+//
+//        Collections.reverse(list);
+//        System.out.println(list.toString()
+//                .replace("[", "")
+//                .replace("]", ""));
+//
+//        ArrayList<Integer> list1 = new ArrayList<>();
+//        Collections.addAll(list1, 1, 3, 2, 5, 4);
+//
+//        list1.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o1 - o2;
+//            }
+//        });
+//
+//        System.out.println(list1.toString()
+//                .replace("[", "")
+//                .replace("]", ""));
+//
+//        list1.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o2 - o1;
+//            }
+//        });
+//
+//        System.out.println(list1.toString()
+//                .replace("[", "")
+//                .replace("]", ""));
+//
+//        ArrayList<Integer> list2 = new ArrayList<>();
+//        Collections.addAll(list2, 1, 3, 7, 5, 6);
+//
+//        list2.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                if ((o2 - o1) % 2 != 0) return 1;
+//                else if ((o2 - o1) % 2 == 0) return -1;
+//                return 0;
+//            }
+//        });
+//
+//        System.out.println(list2.toString()
+//                .replace("[", "")
+//                .replace("]", ""));
+
+//        ArrayList<Integer> list = new ArrayList<>();
+//        ArrayList<Integer> list1 = new ArrayList<>();
+//        ;
+//
+//        for (int i = 0; i < 5; i++) {
+//            list.add(new Random().nextInt(5));
+//            list1.add(new Random().nextInt(5));
+//        }
+//
+//        ArrayList<Integer> list3 = new ArrayList<>(list);
+//        ArrayList<Integer> list4 = new ArrayList<>(list);
+//
+//        System.out.println(list);
+//        System.out.println(list1);
+//        System.out.println("-".repeat(10));
+//
+//        list3.retainAll(list1);
+//        System.out.println(list3);
+//        list4.removeAll(list1);
+//        System.out.println(list4);
+//
+////        list.forEach(n -> System.out.print(n + " "));
+//
+////        for (int n: list) {
+////            System.out.println(n);
+////            list.remove(0);
+////        }
+//
+//        Iterator<Integer> iterator = list.iterator();
+//        while (iterator.hasNext()) {
+//            int n = iterator.next();
+//            System.out.print(n + " ");
+////            iterator.remove();
+//        }
+//
+//        System.out.println("\n" + list);
+//
+//        ListIterator<Integer> iterator1 = list.listIterator();
+//        while (iterator1.hasPrevious()) {
+//            int n = iterator1.previous();
+//            System.out.print(n + " ");
+//        }
+//
+//        int size = list.size();
+//        for (int i = 0; i < size; i++)
+//            list.remove(0);
+//        System.out.println(list);
+
+//        LinkedList<Integer> list5 = new LinkedList<>();
+//        list5.add(4);
+//        list5.getFirst();
+//        list5.getLast();
+
+
+    }
+}
